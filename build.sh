1e8496a5e8989dc4fc19253844c10b76e8d95a1a
djimy <djimy@funpodium.net>
2019-12-12 01:49:36 +0800

    build.sh added

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+cd sa-frontend  && 
+docker build -t localhost:8888/safrontend .  && 
+docker push localhost:8888/safrontend  && 
+cd ..  &&  
+
+cd sa-webapp && 
+docker build -t localhost:8888/sawebapp . && 
+docker push localhost:8888/sawebapp  && 
+cd ..  && 
+
+cd sa-logic  && 
+docker build -t localhost:8888/salogic .  && 
+docker push localhost:8888/salogic  && 
+cd ..
