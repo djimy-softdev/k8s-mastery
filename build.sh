7d8a532ddfda9cb000202929cdf9a34cd233ba70
djimy <djimy@starlordtech.com>
2019-12-12 10:43:58 +0800

    dockerfiles refractored

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-cd sa-frontend  && 
-docker build -t localhost:8888/safrontend .  && 
-docker push localhost:8888/safrontend  && 
-cd ..  &&  
+cd sa-frontend
+docker build -t localhost:8888/safrontend .
+docker push localhost:8888/safrontend
+cd ..
 
-cd sa-webapp && 
-docker build -t localhost:8888/sawebapp . && 
-docker push localhost:8888/sawebapp  && 
-cd ..  && 
+cd sa-webapp
+docker build -t localhost:8888/sawebapp .
+docker push localhost:8888/sawebapp
+cd ..
 
-cd sa-logic  && 
-docker build -t localhost:8888/salogic .  && 
-docker push localhost:8888/salogic  && 
+cd sa-logic
+docker build -t localhost:8888/salogic .
+docker push localhost:8888/salogic
 cd ..
